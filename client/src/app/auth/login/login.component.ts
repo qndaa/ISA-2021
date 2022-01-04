6ad1596c4b64f0fe71cac893f2e1bd7cd6fd9a9f
qndaa <qndaa@uns.ac.rs>
2022-01-04 18:50:02 +0100

    form for login and registration

--- client/src/app/auth/login/login.component.ts
+++ client/src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginForm = new  FormGroup({
+    email: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(4)])
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    // TO DO: Send request for login.
+    console.log(this.loginForm.value);
+  }
+
+  castControl(control: AbstractControl) {
+    return control as FormControl;
+  }
+
+  get emailControl() {
+    return this.loginForm.get('email') as FormControl;
+  }
+
+  get passwordControl() {
+    return this.loginForm.get('password') as FormControl;
   }
 
 }
