d48936e819d1e74f89734aa916f5e8c9570f47e9
Aleksandra Stamenkovic <stamenkovica0@gmail.com>
2022-01-11 23:27:07 +0100

    implemented reservation on front

--- client/src/app/reservation/reservation.component.ts
+++ client/src/app/reservation/reservation.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { ReservationService } from './reservation.service';
 
 @Component({
   selector: 'app-reservation',
   templateUrl: './reservation.component.html',
   styleUrls: ['./reservation.component.css']
 })
+
 export class ReservationComponent implements OnInit {
   adventrueFlag:boolean = false
   boatFlag:boolean = false;
   cottageFlag:boolean = false;
+  listOfEntity: any[] = [];
   reservationAdventure = {
     startDate: new Date,
     startTime:'',
@@ -27,7 +31,7 @@ export class ReservationComponent implements OnInit {
 
   dateStartAdventure:any;
   timeStrAdventure:any;
-  constructor() { }
+  constructor(private reservatioService: ReservationService, private authServie: AuthService) { }
 
   ngOnInit(): void {
   }
@@ -58,12 +62,55 @@ export class ReservationComponent implements OnInit {
   changeDateAdventure(event:any){
     this.reservationAdventure.startDate = new Date(event.target.valueAsNumber);
     this.dateStartAdventure = event.target.value;
+    this.reservationSearch.startDate = new Date(event.target.valueAsNumber);
   }
 
 
   changeTimeAdventure = (event:any) =>{
     let time : string = event.target.value;
     this.reservationAdventure.startTime = time;
+    this.reservationSearch.startTime = time;
+  }
+
+  searachEntity = () => {
+    if(this.adventrueFlag){
+      this.reservatioService.searchAdventure(this.reservationSearch).subscribe(
+        (res:any) => {
+          console.log(res);
+         this.listOfEntity = res;
+        }
+      )
+    }
+    if(this.cottageFlag){
+      this.reservatioService.searchCottage(this.reservationSearch).subscribe(
+        (res:any) => {
+          console.log(res);
+          this.listOfEntity = res;
+        }
+      )
+    }
+    if(this.boatFlag){
+      this.reservatioService.searchShip(this.reservationSearch).subscribe(
+        (res:any) => {
+          console.log(res);
+          this.listOfEntity = res;
+        }
+      )
+    }
+
+  }
+
+
+  reservEtity = (id:any) =>{
+    const a:any = this.authServie.getId();
+    this.reservationAdventure.userId = a.toString() ;
+    this.reservationAdventure.reservationEntityId = id;
+    this.reservatioService.create(this.reservationAdventure).subscribe(
+      (res:any) => {
+        console.log(res);
+        this.listOfEntity = res;
+      }
+    )
   }
 
 
